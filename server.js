2a170c3b5b8d275d8c9d616d0f8245aaf091c8f8
mchail <mchail@gmail.com>
2012-04-14 17:57:39 +0000

    added express, socketio, two routes

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-var http = require('http');
+/*var http = require('http');
 http.createServer(function (req, res) {
    res.writeHead(200, {'Content-Type': 'text/plain'});
    res.end('One big room. Full of <code-slingin\'> bitches.\n');
-}).listen(process.env.PORT);
+}).listen(process.env.PORT);*/
+
+
+var app = require('express').createServer();
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+app.get('/test', function(req, res){
+  res.send('goodbye world');
+});
+
+app.listen(process.env.PORT || 80);
